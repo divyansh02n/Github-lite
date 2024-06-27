01cf21192c8568d9bee0c6e95b723562982f2417
Divyansh <divyansh31n@gmail.com>
2024-06-28 00:07:45 +0530

    dist updated

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import passport from "passport";
 import session from "express-session";
 import path from "path";
-
 import "./passport/github.auth.js";
 
 import userRoutes from "./routes/user.route.js";
@@ -19,26 +18,32 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Here we can remove the cors, it's not necessary in production because the frontend and backend are on the same domain. I forgot to mention that in the video, sorry about that.🙄
+// Uncomment this if you need CORS for development
 // app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
-	console.log(`Server started on http://localhost:${PORT}`);
-	connectMongoDB();
+  console.log(`Server started on http://localhost:${PORT}`);
+  connectMongoDB()
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error("Failed to connect to MongoDB", err);
+    });
 });
